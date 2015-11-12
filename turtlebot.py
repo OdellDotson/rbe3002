ab1e3cc04a6a0e97473a6ebd86ca24e15d2f80e3
thhughes <Troy.h.hughes@gmail.com>
2015-11-12 15:14:56 -0500

    Fixed turning bug

--- turtlebot.py
+++ turtlebot.py
@@ -19,7 +19,7 @@ from communicator import communicator
 #Message Types
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-
+from geometry_msgs.msg import PoseStamped
 
 class turtlebot(log_base,communicator):
     def __init__(self, name, wheelbase=0.352):
@@ -42,19 +42,18 @@ class turtlebot(log_base,communicator):
         self._vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
 
         self._odom_sub = rospy.Subscriber('/odom', Odometry, self.odomCallback, queue_size=3)
+        # self._map_sub = rospy.Subscriber('/map',PoseStamped,self.mapCallback, queue_size=3)
         # self._bmp_sub = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, turtlebot.setBumper, queue_size=3)
 
         #### Private variables that get updated by callbacks.
         ## Odom Handlers ##
-        self._odom_listener = tf.TransformListener()
+        self._odom_list = tf.TransformListener()
         self._odom_tf = tf.TransformBroadcaster()
         self._odom_tf.sendTransform((0, 0, 0),(0, 0, 0, 1),rospy.Time.now(),"base_footprint","odom")
         ## Map Handlers ##
         self._map_list = tf.TransformListener()
-        self._map_tf = tf.TransformBroadcaster()
         ## Goal Handlers ##
         self._goal_list = tf.TransformListener()
-        self._goal_tf = tf.TransformBroadcaster()
 
         #### Private robot Information
         self._wheelbase = wheelbase
@@ -77,8 +76,7 @@ class turtlebot(log_base,communicator):
 
     def odomCallback(self, msg):
         """
-        This will unpack the quaternian style message into an x,y,z in the map
-        and the quaternian x,y,z,w
+        This will handle the odom frame location and information
 
         :param msg: this is a message form the odom subscriber
         :return: None
@@ -88,6 +86,17 @@ class turtlebot(log_base,communicator):
         self._quatx, self._quaty, self._quatz, self._quatw = quat
         self._quat = (self._quatx, self._quaty, self._quatz, self._quatw)
 
+    # def mapCallback(self,msg):
+    #     """
+    #     This will handle the map frame location of the robot.
+    #
+    #     :param msg: this is a message form the odom subscriber
+    #     :return: None
+    #     """
+    #     pos,quat = self._quatFromMsg(msg)
+    #     self._mapx, self._mapy, self._mapz = pos
+    #     self._map_quatx, self._map_quaty, self._map_quatz, self._map_quatw = quat
+    #     self._map_quat = (self._map_quatx, self._map_quaty, self._map_quatz, self._map_quatw)
 
     """------------------General Movement------------------"""
     """ Movement Helpers """
@@ -151,7 +160,7 @@ class turtlebot(log_base,communicator):
                 regulated_speed = speed*(0.2) + speed*(dist_so_far / distance)
             elif dist_so_far >= abs(distance)*0.75:
                 print 'Here'
-                regulated_speed = speed(0.2) + speed*(1- (dist_so_far / distance))
+                regulated_speed = speed*(0.2) + speed*(1- (dist_so_far / distance))
             else:
                 regulated_speed = speed
             if distance > 0: self._spinWheels(regulated_speed,regulated_speed,0.1)
@@ -161,6 +170,8 @@ class turtlebot(log_base,communicator):
             arrived = dist_so_far >= abs(distance)
 
         self._stopRobot()
+
+
     def rotate(self, angle, speed = 0.05):
         """
         :param angle: <int or double> the angle the robot should turn in degrees
@@ -173,21 +184,28 @@ class turtlebot(log_base,communicator):
 
         angle_to_travel_rad = math.radians(angle)
         start_theta_rad = tools.normalizeTheta(self._quat)
+        print angle_to_travel_rad, start_theta_rad
+        print ("angle to travel", "start theta", "Current theta", "d_theta")
 
         while not done and (not rospy.is_shutdown()):
             current_theta_rad = tools.normalizeTheta(self._quat)
 
             # Calculates the difference in the thetas
             # This keeps track of
-            if start_theta_rad > current_theta_rad and start_theta_rad > math.pi and current_theta_rad < math.pi:
+            debug_state = None
+            if start_theta_rad > current_theta_rad and start_theta_rad > math.pi*3/2 and current_theta_rad < math.pi/2:
                 d_theta = ((2*math.pi) - start_theta_rad) + current_theta_rad
-            elif start_theta_rad < current_theta_rad and start_theta_rad < math.pi and current_theta_rad > math.pi:
+                debug_state = 1
+            elif start_theta_rad < current_theta_rad and start_theta_rad < math.pi/2 and current_theta_rad > math.pi*3/2:
                 d_theta = ((2*math.pi) - current_theta_rad) + start_theta_rad
+                debug_state = 2
             else:
                 d_theta = start_theta_rad - current_theta_rad
+                debug_state = 3
 
+            print angle_to_travel_rad, start_theta_rad, current_theta_rad, d_theta, debug_state
             ## If you're within 0.1 Radian stop
-            if angle_to_travel_rad - d_theta < 0.1:
+            if abs(angle_to_travel_rad) - abs(d_theta) < 0.1:
                 done = True
                 self._stopRobot()
             else:
@@ -210,8 +228,19 @@ class turtlebot(log_base,communicator):
 
     def main(self):
         try:
+            try:
+                self._odom_list.waitForTransform('map', 'base_footprint', rospy.Time(0), rospy.Duration(4.0))
+            except tf.Exception:
+                print "Exception thrown"
+                return
+            p,q = self._map_list.lookupTransform("map","base_footprint",rospy.Time(0))
+            print p,q
             self.rotate(90)
+            p,q = self._map_list.lookupTransform("map","base_footprint",rospy.Time(0))
+            print p,q
             self.driveStraight(1,1)
+            p,q = self._map_list.lookupTransform("map","base_footprint",rospy.Time(0))
+            print p,q
         except rospy.ROSInterruptException:
             pass
 
